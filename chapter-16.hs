81c8e8a866bd8dcbbb3421b2a90aab847d188b3f
Suvrat Apte <suvratapte@gmail.com>
2021-10-19 16:51:40 +0200

    Add some exercises from chapter 16

---
+++ chapter-16.hs
@@ -0,0 +1,76 @@
+module Chapter_16 where
+
+-- Exercises: Be Kind
+
+{-
+Given a type signature, determine the kinds of each type variable:
+
+1. What’s the kind of a?
+a -> a
+
+a :: *
+
+2. What are the kinds of b and T ? (The T is capitalized on purpose!)
+a -> b a -> T (b a)
+
+T :: * -> *
+
+3. What’s the kind of 𝑐?
+c a b -> c b a
+
+c :: * -> * -> *
+-}
+
+-- Wait, how does that even typecheck?
+
+{-
+
+(.) :: (b -> c) -> (a -> b) -> a -> c
+
+fmap :: Functor f => (m -> n) -> f m -> f n
+
+fmap :: Functor g => (x -> y) -> g x -> g y
+
+Substituting both fmap signatures in dot (.) :
+
+((m -> n) -> f m -> f n) -> ((x -> y) -> g x -> g y) -> a -> c
+-----------------------     ------------------------
+      b -> c                        a -> b
+
+From this we can infer:
+
+b ~ m -> n
+c ~ f m -> f n
+
+a ~ x -> y
+b ~ g x -> g y
+
+From this we can infer:
+
+b ~ m -> n ~ g x -> g y
+
+m ~ g x
+n ~ g y
+
+Subsituting values for a, c :
+
+((m -> n) -> f m -> f n) -> ((x -> y) -> g x -> g y) -> (x -> y) -> (f m -> f n)
+-----------------------     ------------------------    --------    ------------
+      b -> c                        a -> b                 a              c
+
+Substuting values for m, n :
+
+((g x -> g y) -> (f g x) -> (f g y)) -> ((x -> y) -> (g x -> g y)) -> (x -> y) -> (f (g x) -> f (g y))
+------------------------------------    --------------------------    --------    --------------------
+            b -> c                              a -> b                    a                c
+
+So when you use (fmap . fmap), the first two arguments are gives so `b -> c` and
+`a -> b` is applied so the signature of the resulting function will be:
+
+(x -> y) -> f (g x) -> f (g y)
+
+Which matches the type you get when you write `:t (fmap . fmap)` in ghci.
+
+That's how this type checks!
+
+-}
