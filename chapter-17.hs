062cc162c3167517af242a3e8563d704eb9e0acf
Suvrat Apte <suvratapte@gmail.com>
2021-10-25 22:57:55 +0200

    Add more exercises from chapter 17

--- chapter-17.hs
+++ chapter-17.hs
@@ -6,6 +6,9 @@ module Chapter_17 where
 
 import Data.List (elemIndex)
 import Control.Applicative
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
 -- Exercises: Lookups
 
@@ -89,3 +92,68 @@ instance Functor (Constant a) where
 instance Monoid a => Applicative (Constant a) where
   pure x = Constant mempty
   Constant f <*> Constant x = Constant $ f <> x
+
+-- Exercise: Fixer Upper
+
+{-
+Given the function and values provided, use (<$>) from Functor, (<*>) and pure
+from the Applicative typeclass to fill in missing bits of the broken code to
+make it work.
+-}
+
+-- 1
+
+x4 = const <$> Just "Hello" <*> pure "World"
+
+-- 2
+y4 = (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1, 2, 3]
+
+-- List Applicative Exercise
+
+{-
+Implement the list Applicative. Writing a minimally complete Applicative
+instance calls for writing the definitions of both pure and <*>. We’re going to
+provide a hint as well. Use the checkers library to validate your Applicative
+instance.
+-}
+
+data List a = Nil | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Semigroup (List a) where
+  Nil <> Nil = Nil
+  Nil <> x = x
+  x <> Nil = x
+  Cons x xs <> y = Cons x $ xs <> y
+
+instance Monoid (List a) where
+  mempty = Nil
+  mappend = (<>)
+
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) $ fmap f xs
+
+instance Applicative List where
+  pure x = Cons x Nil
+  Nil <*> _ = Nil
+  _ <*> Nil = Nil
+  (Cons f fs) <*> xs = fmap f xs `mappend` (fs <*> xs)
+
+{-
+Expected result:
+Prelude> let f = Cons (+1) (Cons (*2) Nil)
+Prelude> let v = Cons 1 (Cons 2 Nil)
+Prelude> f <*> v
+Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil)))
+-}
+
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = frequency [ (1, return Nil),
+                          (3, Cons <$> arbitrary <*> arbitrary) ]
+
+instance Eq a => EqProp (List a) where
+  (=-=) = eq
+
+-- To test with quickBatch:
+-- quickBatch . applicative $ Cons ("1", "2", "3") Nil
